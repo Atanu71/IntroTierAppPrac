0d1498b72fb10eadc2cdc21cf3e110013e6a15ac
Atanu Das <dasatanu348@gmail.com>
2023-03-26 20:59:15 +0600

    Initial commit

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroTierAppPrac.Controllers
+{
+    public class EmpController : ApiController
+    {
+        [HttpGet]
+        [Route("api/employees")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+              return  Request.CreateResponse(HttpStatusCode.OK, EmployeeService.Get());
+            }
+            catch(Exception ex)
+            {
+                 return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntroTierAppPrac.WebApiApplication" Language="C#" %>
